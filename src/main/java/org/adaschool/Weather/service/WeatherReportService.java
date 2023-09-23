1b194a7696344e6b918900115b623841294c8e4a
edwar.lozano <edwar.lozano@mail.escuelaing.edu.co>
2023-09-22 23:59:06 -0500

    Completado

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "48cec3ffc742e75a3a69a806e6a57528";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
